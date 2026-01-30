3995b1530c77370025b1ba9720196bd5868f7146
Альбина Исмаилова <aismailova@it.gov.kg>
2026-01-30 15:07:33 +0600

    fixed file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { myRouter } from './router'; // сен түзгөн router
+import { myRouter } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
